2ceed86e717136c8cbf2bccbe404210d48fa1511
Charles Yach <CharlesY712@gmail.com>
2017-12-17 15:01:42 -0700

    Turning idea into object using prototypical method

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,46 @@
+var $ideaTitle = $('[name=idea-title]');
+var $ideaDescription = $('[name=idea-description');
 
-$('#save-button').click(function(event) {
+function Idea (id, title, body) {
+  this.id = id;
+  this.title = title;
+  this.body = body;
+  this.quality = 'quality: swill';
+}
 
-  var $ideaTitle = $('[name=idea-title]');
-  var $ideaDescription = $('[name=idea-description');
-
-  var idea = 
+Idea.prototype.prependIdea = function() {
+  $('#idea-list').prepend(
     `<article>
-      <h2>${$ideaTitle.val()}</h2>
+      <h2>${this.title}</h2>
       <label for="remove-button">
         <button class="remove button"></button>
       </label>
-      <p>${$ideaDescription.val()}</p>
+      <p>${this.body}</p>
       <label for="quality-up-button">
         <button class="quality-up button" name="quality-up-button"></button>
       </label>
       <label for="quality-down-button">
         <button class="quality-down button" name="quality-down-button"></button>
       </label>
-      <h3>quality: swill</h3>
+      <h3>${this.quality}</h3>
     </article>`
+  );
+};
 
+$('#save-button').click(function(event) {
+  var idea = new Idea (Date.now(), $ideaTitle.val(), $ideaDescription.val());
+  
   if (`${$ideaTitle.val()}` == "" || `${$ideaDescription.val()}` == ""){
     return false;
   } else {
     event.preventDefault();
-    $('#idea-list').prepend(idea);
+    idea.prependIdea();
     $ideaTitle.val('');
     $ideaDescription.val('');
     $ideaTitle.focus();
   }
-
 });
+
 
 $('#idea-list').on('click', '.remove', function(e) {
  $(this).closest('article').fadeOut(function() {
@@ -55,4 +65,42 @@ $(this).closest('label').siblings('h3').text('quality: plausible');
 });
 
 
+
+
+
+
+
+
+// $('#save-button').click(function(event) {
+
+//   var $ideaTitle = $('[name=idea-title]');
+//   var $ideaDescription = $('[name=idea-description');
+
+//   var idea = 
+//     `<article>
+//       <h2>${$ideaTitle.val()}</h2>
+//       <label for="remove-button">
+//         <button class="remove button"></button>
+//       </label>
+//       <p>${$ideaDescription.val()}</p>
+//       <label for="quality-up-button">
+//         <button class="quality-up button" name="quality-up-button"></button>
+//       </label>
+//       <label for="quality-down-button">
+//         <button class="quality-down button" name="quality-down-button"></button>
+//       </label>
+//       <h3>quality: swill</h3>
+//     </article>`
+
+//   if (`${$ideaTitle.val()}` == "" || `${$ideaDescription.val()}` == ""){
+//     return false;
+//   } else {
+//     event.preventDefault();
+//     $('#idea-list').prepend(idea);
+//     $ideaTitle.val('');
+//     $ideaDescription.val('');
+//     $ideaTitle.focus();
+//   }
+
+// });
 
