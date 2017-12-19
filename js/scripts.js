c3cc0f3c7639d7aac82fad41d3917432cd420971
Charles Yach <CharlesY712@gmail.com>
2017-12-19 09:23:25 -0700

    Adding save to storage andpage load from storage

--- js/scripts.js
+++ js/scripts.js
@@ -8,52 +8,58 @@ function Idea (id, title, body) {
   this.quality = 'quality: swill';
 }
 
-Idea.prototype.prependIdea = function() {
+function prependIdea (idea) {
   $('#idea-list').prepend(
     `<article>
-      <h2>${this.title}</h2>
-      <label for="remove-button">
-        <button class="remove button"></button>
-      </label>
-      <p>${this.body}</p>
-      <label for="quality-up-button">
-        <button class="quality-up button" name="quality-up-button"></button>
-      </label>
-      <label for="quality-down-button">
-        <button class="quality-down button" name="quality-down-button"></button>
-      </label>
-      <h3>${this.quality}</h3>
+    <h2>${idea.title}</h2>
+    <label for="remove-button">
+    <button class="remove button"></button>
+    </label>
+    <p>${idea.body}</p>
+    <label for="quality-up-button">
+    <button class="quality-up button" name="quality-up-button"></button>
+    </label>
+    <label for="quality-down-button">
+    <button class="quality-down button" name="quality-down-button"></button>
+    </label>
+    <h3>${idea.quality}</h3>
     </article>`
-  );
+    );
 };
 
 $('#save-button').click(function(event) {
   var idea = new Idea (Date.now(), $ideaTitle.val(), $ideaDescription.val());
-  
-  console.log(idea.id)
 
   if (`${$ideaTitle.val()}` == "" || `${$ideaDescription.val()}` == ""){
     return false;
   } else {
-    event.preventDefault();
-    idea.prependIdea();
-    $ideaTitle.val('');
-    $ideaDescription.val('');
-    $ideaTitle.focus();
-    storeIdea(idea.id, idea.title, idea.description, idea.quality);
-    // var stringifiedObject = JSON.stringify(idea);
-    // localStorage.setItem(idea.id , stringifiedObject);
-    // console.log(idea.id);
-
-  }
+   event.preventDefault();
+   prependIdea(idea);
+   toLocalStorage(idea);
+   $ideaTitle.val('');
+   $ideaDescription.val('');
+   $ideaTitle.focus();
+ }
 });
 
-function storeIdea(id, title, body, quality) {
-  var objectToStore = new Idea (id, title, body, quality);
-  var stringifiedObject = JSON.stringify(objectToStore);
-  localStorage.setItem(id , stringifiedObject);
+function toLocalStorage(idea) {
+ var stringifiedIdea = JSON.stringify(idea);
+ localStorage.setItem(idea.id, stringifiedIdea);
 }
 
+
+function pageLoad() {
+ for (var i = 0; i < localStorage.length; i++) {
+   var returnIdea = localStorage.getItem(localStorage.key(i));
+   var parseIdea = JSON.parse(returnIdea);
+   prependIdea(parseIdea)
+   console.log(parseIdea);
+ }
+}
+
+window.onload = function() {
+ pageLoad();
+}
 
 $('#idea-list').on('click', '.remove', function(e) {
  $(this).closest('article').fadeOut(function() {
@@ -62,18 +68,18 @@ $('#idea-list').on('click', '.remove', function(e) {
 });
 
 $('#idea-list').on('click', '.quality-up', function(e) {
-if ($(this).closest('label').siblings('h3').text() === ('quality: swill')) {
-$(this).closest('label').siblings('h3').text('quality: plausible');
-} else if ($(this).closest('label').siblings('h3').text() === ('quality: plausible')) {
- $(this).closest('label').siblings('h3').text('quality: genius');
-} 
+  if ($(this).closest('label').siblings('h3').text() === ('quality: swill')) {
+    $(this).closest('label').siblings('h3').text('quality: plausible');
+  } else if ($(this).closest('label').siblings('h3').text() === ('quality: plausible')) {
+   $(this).closest('label').siblings('h3').text('quality: genius');
+ } 
 });
 
 $('#idea-list').on('click', '.quality-down', function(e) {
-if ($(this).closest('label').siblings('h3').text() === ('quality: genius')) {
-$(this).closest('label').siblings('h3').text('quality: plausible');
-} else if ($(this).closest('label').siblings('h3').text() === ('quality: plausible')) {
- $(this).closest('label').siblings('h3').text('quality: swill');
-} 
+  if ($(this).closest('label').siblings('h3').text() === ('quality: genius')) {
+    $(this).closest('label').siblings('h3').text('quality: plausible');
+  } else if ($(this).closest('label').siblings('h3').text() === ('quality: plausible')) {
+   $(this).closest('label').siblings('h3').text('quality: swill');
+ } 
 });
 
