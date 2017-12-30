2a45b0242db4ec3a548d6566d16a6900d1e0261b
Chase <hmmchase@gmail.com>
2017-12-29 18:34:59 -0700

    Refactor pageLoad and upVote/downVote functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,51 @@
-pageLoad();
-
-$('#save-button').on('click', makeIdea);
+$('.save-button').click(makeIdea);
 $('.search-field').on('keyup', search);
+$('#idea-list').on('click', 'h2', editContentTitle);
+$('#idea-list').on('click', 'p', editContentBody);
 $('#idea-list').on('click', '.remove', removeIdea);
-$('#idea-list').on('click', 'h2', editContent);
-$('#idea-list').on('click', 'p', editContent);
-$('#idea-list').on('click', '.quality-down', downVote);
 $('#idea-list').on('click', '.quality-up', upVote);
+$('#idea-list').on('click', '.quality-down', downVote);
+
+$(document).ready(pageLoad);
+
+function pageLoad() {
+  Object.keys(localStorage).forEach(function (value) {
+    prependIdea(JSON.parse(localStorage.getItem(value)));
+  });
+};
 
 function makeIdea(event) {
-  var $ideaTitle = $('.idea-title]');
+  var $ideaTitle = $('.idea-title');
   var $ideaDescription = $('.idea-description');
-  var idea = new Idea (Date.now(), $ideaTitle.val(), $ideaDescription.val());
-  if (`${$ideaTitle.val()}` == "" || `${$ideaDescription.val()}` == ""){
+  var idea = new Idea(Date.now(), $ideaTitle.val(), $ideaDescription.val(), 'Swill');
+
+  if (`${$ideaTitle.val()}` == "" || `${$ideaDescription.val()}` == "") {
     return false;
   } else {
-  event.preventDefault();
-  prependIdea(idea);
-  toLocalStorage(idea);
-  inputReset();
- }};
 
-function Idea (id, title, body) {
+    event.preventDefault();
+    prependIdea(idea);
+    toLocalStorage(idea);
+    inputReset();
+  }
+};
+
+function Idea(id, title, body, quality, count) {
   this.id = id;
   this.title = title;
   this.body = body;
-  this.quality = 'quality: swill';
+  this.quality = quality;
+  this.count = count || 0;
 }
 
 function prependIdea(idea) {
   $('#idea-list').prepend(
     `<article id="${idea.id}">
-      <h2>${idea.title}</h2>
-      <button class="remove button label"></button>
-      <p class="idea-body">${idea.body}</p>
-      <button class="quality-up button label" name="quality-up-button"></button>
-      <button class="quality-down button label" name="quality-down-button"></button>
+      <h2 contenteditable="true">${idea.title}</h2>
+      <button class="remove button"></button>
+      <p class="idea-body" contenteditable="true">${idea.body}</p>
+      <button class="quality-up arrow button"></button>
+      <button class="quality-down arrow button"></button>
       <p class="quality">${idea.quality}</p>
     </article>`
   );
@@ -43,78 +53,79 @@ function prependIdea(idea) {
 
 function search() {
   var searchRequest = $('.search-field').val();
-  $('article').each(function(){
+  $('article').each(function () {
     var searchResult = $(this).text().indexOf(searchRequest);
-    this.style.display = (searchResult > -1) ? "" : "none";
-  });
-};
-
-function inputReset() {
-  $('.idea-title').val('');
-  $('.idea-description').val('');
-  $('.idea-title').focus();
-};
-
-function toLocalStorage(idea) {
- localStorage.setItem(idea.id, JSON.stringify(idea));
-};
-
-////////Fix key argument
-function pageLoad() {
-  var returnIdea = localStorage.getItem(localStorage.key(id));
-  var parseIdea = JSON.parse(returnIdea);
-  parseIdea.forEach(function(val, i){
-    prependIdea(val);
+    this.style.display = searchResult > -1 ? "" : "none";
   })
 };
 
-function editContent () {
-  $(this).prop('contenteditable', true).focus();
-  $(this).focusout(function() {
-    var key = $(this).closest('article').attr('id')
+function inputReset() {
+  var $ideaTitle = $('.idea-title');
+  var $ideaDescription = $('.idea-description');
+  $ideaTitle.val('');
+  $ideaDescription.val('');
+  $ideaTitle.focus();
+};
+
+function toLocalStorage(idea) {
+  localStorage.setItem(idea.id, JSON.stringify(idea));
+};
+
+function editContentTitle() {
+  // editContent(title);
+  $(this).focusout(function () {
+    var key = $(this).closest('article').attr('id');
     var retrievedIdea = localStorage.getItem(key);
     var parsedIdea = JSON.parse(retrievedIdea);
     parsedIdea['title'] = $(this).html();
+    var stringifiedObject = JSON.stringify(parsedIdea);
+    localStorage.setItem(key, stringifiedObject);
+  })
+};
+
+function editContentBody() {
+  // editContent(body);
+  $(this).focusout(function () {
+    var key = $(this).closest('article').attr('id');
+    var retrievedIdea = localStorage.getItem(key);
+    var parsedIdea = JSON.parse(retrievedIdea);
     parsedIdea['body'] = $(this).html();
     var stringifiedObject = JSON.stringify(parsedIdea);
     localStorage.setItem(key, stringifiedObject);
-    })
-  };
-
-function removeIdea() {
- $(this).closest('article').fadeOut(function() {
-   $(this).remove();
- });
- localStorage.removeItem($(this).closest('article').attr('id'));
+  })
 };
 
+function removeIdea() {
+  $(this).closest('article').fadeOut(function () {
+    $(this).remove();
+  })
+  localStorage.removeItem($(this).closest('article').attr('id'));
+};
+
+var arrayQuality = ["Swill", "Plausible", "Genius"];
+
 function upVote() {
-  var key = $(this).closest('article').attr('id')
-  var retrievedIdea = localStorage.getItem(key);
-  var parsedIdea = JSON.parse(retrievedIdea);
-  if (parsedIdea['quality'] === ('quality: swill')) {
-    $(this).closest('label').siblings('h3').text('quality: plausible');
-    parsedIdea['quality'] = 'quality: plausible';
-    toLocalStorage(parsedIdea);
-  } else if (parsedIdea['quality'] === ('quality: plausible')) {
-    $(this).closest('label').siblings('h3').text('quality: genius');
-    parsedIdea['quality'] = 'quality: genius';
-    toLocalStorage(parsedIdea);
- } 
+  var article = $(this).closest("article");
+  var ideaID = $(this).closest('article').attr("id");
+  var parsedIdea = JSON.parse(localStorage.getItem(ideaID));
+
+  if (parsedIdea.count < 2) {
+    parsedIdea.count++;
+    parsedIdea.quality = arrayQuality[parsedIdea.count];
+    article.find('.quality').text(arrayQuality[parsedIdea.count]);
+  }
+  localStorage.setItem(ideaID, JSON.stringify(parsedIdea));
 };
 
 function downVote() {
-  var key = $(this).closest('article').attr('id')
-  var retrievedIdea = localStorage.getItem(key);
-  var parsedIdea = JSON.parse(retrievedIdea);
-  if (parsedIdea['quality'] === ('quality: genius')) {
-    $(this).closest('label').siblings('h3').text('quality: plausible');
-    parsedIdea['quality'] = 'quality: plausible';
-    toLocalStorage(parsedIdea);
-  } else if (parsedIdea['quality'] === ('quality: plausible')) {
-   $(this).closest('label').siblings('h3').text('quality: swill');
-   parsedIdea['quality'] = 'quality: swill';
-   toLocalStorage(parsedIdea);
- } 
-};
+  var article = $(this).closest("article");
+  var ideaID = $(this).closest('article').attr("id");
+  var parsedIdea = JSON.parse(localStorage.getItem(ideaID));
 
+  if (parsedIdea.count > 0) {
+    parsedIdea.count--;
+    parsedIdea.quality = arrayQuality[parsedIdea.count];
+    article.find('.quality').text(arrayQuality[parsedIdea.count]);
+  }
+  localStorage.setItem(ideaID, JSON.stringify(parsedIdea));
+};
